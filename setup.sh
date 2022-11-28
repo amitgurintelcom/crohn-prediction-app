bfece702dcc69d1a04ec58218bd0cdd8ce90d04c
amitgurintelcom <50952358+amitgurintelcom@users.noreply.github.com>
2022-11-28 18:43:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-export api_key_aws=gtUiWpLQz8rfehM9JWGLgGYo
-export api_key_intel=EbpEhXxmCnTJRmT5mQtoFHhd
-export conn_addr_aws=ecg-aws-1.am22ensuxenodo5ihblszm8.cloud.cnvrg.io
-export conn_addr_intel=ecg-web-dev-cloud-1.aaorm9bej4xwhihmdknjw5e.cloud.cnvrg.io
-export conn_req_aws=/api/v1/endpoints/gr1eyvrcq4jg1mdpkhtw
-export conn_req_intel=/api/v1/endpoints/zgj4rgpyeepc246snbpw
+export api_key_aws=3zFifoikZad4SDRLJFsoVX9F
+export api_key_intel=3zFifoikZad4SDRLJFsoVX9F
+export conn_addr_aws=inference-33-1.aaorm9bej4xwhihmdknjw5e.cloud.cnvrg.io
+export conn_addr_intel=inference-33-1.aaorm9bej4xwhihmdknjw5e.cloud.cnvrg.io
+export conn_req_aws=/api/v1/endpoints/p6ktydnwfegxf8azpano
+export conn_req_intel=/api/v1/endpoints/p6ktydnwfegxf8azpano
 mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
