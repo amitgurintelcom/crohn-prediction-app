600efab0a47d70c2a24c924a2821d2b3195ee4b9
amitgurintelcom <50952358+amitgurintelcom@users.noreply.github.com>
2022-11-28 18:41:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+export api_key_aws=gtUiWpLQz8rfehM9JWGLgGYo
+export api_key_intel=EbpEhXxmCnTJRmT5mQtoFHhd
+export conn_addr_aws=ecg-aws-1.am22ensuxenodo5ihblszm8.cloud.cnvrg.io
+export conn_addr_intel=ecg-web-dev-cloud-1.aaorm9bej4xwhihmdknjw5e.cloud.cnvrg.io
+export conn_req_aws=/api/v1/endpoints/gr1eyvrcq4jg1mdpkhtw
+export conn_req_intel=/api/v1/endpoints/zgj4rgpyeepc246snbpw
 mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
