a63027b22005e371ba73b29bc275720eb559ca8a
gal.sabina.star <gal.sabina.star@intel.com>
2022-12-05 14:43:52 +0200

    Add preview of the video

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid.shared import JsCode
+from moviepy.editor import VideoFileClip
 ###################################
 from PIL import Image
 import io
@@ -28,6 +29,16 @@ def _max_width_():
     """,
         unsafe_allow_html=True,
     )
+
+def present_video(content):
+    if not os.path.exists("myvideo.mp4"):
+        videoClip = VideoFileClip(content).subclip(0,60)
+        clip_resized = videoClip.resize(height=360)
+        clip_resized.write_videofile("myvideo.mp4", fps=25)
+
+    with open('myvideo.mp4', 'rb') as f:
+        vid = f.read()
+    st.video(vid)
 
 def select_host(selected):
     if selected=="AWS":
@@ -56,6 +67,11 @@ def select_host(selected):
 st.set_page_config(page_icon="✂️", page_title="Crohn's Treatment Outcome Prediction", layout="wide")
 
 with st.sidebar:
+        st.image('./intel.png', width=90)
+        st.subheader('Developer Cloud')
+        st.image("footer.png")
+        st.write("")
+        st.write("")
         selected = option_menu(
             menu_title="Choose web host",  # required
             options=["Intel DevCloud", "AWS (Future)", "Azure (Future)"],  # required
@@ -65,54 +81,47 @@ with st.sidebar:
                 )
         st.info(f'web host is {selected}', icon="ℹ️")
 
-c1_1, c1_2, _, _ = st.columns([3.5, 8, 8, 8])
-with c1_1:
-    st.image('./intel.png', width=100)
-with c1_2:
-    st.subheader('Developer Cloud')
 
-c1, c2 = st.columns([5,5])
+st.title("Crohn's Treatment Outcome Prediction")
+c1, c2 = st.columns([1,5])
 with c1:
-    st.title("Crohn's Treatment Outcome Prediction")
-    st.write('''
-    Capsule endoscopy (CE) is a prime modality for diagnosing and monitoring Crohn's disease (CD). 
-    However, CE's video wasn't utilized for predicting the success of biologic therapy.
-    This demo runs the Timesformer model trained on Sheba data by Intel's New AI technologies group.
-    The model reaches a 20% improvement over the best existing biomarker (fecal calprotectin) while providing decision explainability and confidence level
-    ''')
     st.image('CE_basic.jpg', width=200)
-    st.image('footer.png')
-
 with c2:
-    api_key, conn_addr, conn_req = select_host(selected)
-    uploaded_file = st.file_uploader("", type="mpg", key="1")
-    if uploaded_file is not None:
-        content = uploaded_file.read()
-        # encoded_string = base64.b64encode(content).decode("utf-8")
-        video_url="https://libhub-readme.s3.us-west-2.amazonaws.com/crohns-app-cvbock/video.mpeg"
-        st.video(video_url)
-        request_dict =  {"video":video_url}
-        payload = '{"input_params":' + json.dumps(request_dict) + "}"
-        headers = {
+    st.write('''
+        Capsule endoscopy (CE) is a prime modality for diagnosing and monitoring Crohn's disease (CD). 
+        However, CE's video wasn't utilized for predicting the success of biologic therapy.
+        This demo runs the Timesformer model trained on Sheba data by Intel's New AI technologies group.
+        The model reaches a 20% improvement over the best existing biomarker (fecal calprotectin) while providing decision explainability and confidence level
+        ''')
+
+api_key, conn_addr, conn_req = select_host(selected)
+uploaded_file = st.file_uploader("", type="mpg", key="1")
+if uploaded_file is not None:
+    content = uploaded_file.read()
+    encoded_string = base64.b64encode(content).decode("utf-8")
+    video_url="https://libhub-readme.s3.us-west-2.amazonaws.com/crohns-app-cvbock/video.mpeg"
+    request_dict =  {"video":video_url}
+    payload = '{"input_params":' + json.dumps(request_dict) + "}"
+    headers = {
             'Cnvrg-Api-Key': api_key,
             'Content-Type': "application/json"
             }
 
-    if uploaded_file is not None:
-        file_container = st.expander("Check your uploaded .csv")
-        with st.spinner('This might take few seconds ... '):
-            try:
-                conn = http.client.HTTPSConnection(conn_addr)
-                st.info('Sending File to the server')
-                conn.request("POST", conn_req, payload, headers)
-                st.info('Got server POST response')
-                res = conn.getresponse()
-                data = res.read()
-                output = data.decode("utf-8")
-            except: 
-                st.error('Cant connect to server. Try to disable VPN!')
-                is_exception_raised = True
-                output = None
+if uploaded_file is not None:
+    file_container = st.expander("Check your uploaded .csv")
+    with st.spinner('This might take few seconds ... '):
+        try:
+            conn = http.client.HTTPSConnection(conn_addr)
+            st.info('Sending File to the server')
+            conn.request("POST", conn_req, payload, headers)
+            st.info('Got server POST response')
+            res = conn.getresponse()
+            data = res.read()
+            output = data.decode("utf-8")
+        except: 
+            st.error('Cant connect to server. Try to disable VPN!')
+            is_exception_raised = True
+            output = None
 
     if not is_exception_raised and output is not None:
         #print(output)
@@ -127,16 +136,18 @@ with c2:
             conf_win= float(confidence[0])*100
         print("prediction:", prediction)
         images = json.loads(output)['prediction'][2]['images']
-
-        ### Display results
-        st.metric(label="Diseases Prediction:", value=f"{pred_str}")
-        st.metric(label=f'confidence level', value=f"{conf_win}%")
-        for i,image_1 in enumerate(images):
-            image_data = base64.b64decode(image_1) 
-            image_2 = Image.open(io.BytesIO(image_data))
-            st.image(image_2, width=500)
-        ###
-        # Add images
+    
+        c1, c2 = st.columns([3,5])
+        with c1:
+            with st.spinner('Loading now the input video ...'):
+                present_video(video_url)
+        with c2:
+            st.metric(label="Diseases Prediction:", value=f"{pred_str}")
+            st.metric(label=f'confidence level', value=f"{conf_win}%")
+            for i,image_1 in enumerate(images):
+                image_data = base64.b64decode(image_1) 
+                image_2 = Image.open(io.BytesIO(image_data))
+                st.image(image_2, width=500)
     else:
         st.info(f"""👆 Please upload a .mpg Capsule Endoscopy video first""")
         st.stop()
